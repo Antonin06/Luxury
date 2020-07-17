ea9871ca4a44803bf0cc48e45ef14a52000cbb97
Antonin06 <avon.antonin@gmail.com>
2020-07-17 13:54:00 +0200

    maj roles

--- src/Entity/Candidate.php
+++ src/Entity/Candidate.php
@@ -26,11 +26,6 @@ class Candidate implements UserInterface
     private $email;
 
     /**
-     * @ORM\Column(type="json")
-     */
-    private $roles = [];
-
-    /**
      * @var string The hashed password
      * @ORM\Column(type="string")
      */
@@ -151,6 +146,11 @@ class Candidate implements UserInterface
      */
     private $files;
 
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $is_admin;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -183,19 +183,14 @@ class Candidate implements UserInterface
      */
     public function getRoles(): array
     {
-        $roles = $this->roles;
+        if($this->getIsAdmin()){
+          $roles[] = 'ROLE_ADMIN';
+        }
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
-
         return array_unique($roles);
     }
 
-    public function setRoles(array $roles): self
-    {
-        $this->roles = $roles;
-
-        return $this;
-    }
 
     /**
      * @see UserInterface
@@ -501,6 +496,18 @@ class Candidate implements UserInterface
     public function setFiles(?string $files): self
     {
         $this->files = $files;
+
+        return $this;
+    }
+
+    public function getIsAdmin(): ?bool
+    {
+        return $this->is_admin;
+    }
+
+    public function setIsAdmin(?bool $is_admin): self
+    {
+        $this->is_admin = $is_admin;
 
         return $this;
     }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -20,6 +20,7 @@ class RegistrationFormType extends AbstractType
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
+                'required' => false,
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
